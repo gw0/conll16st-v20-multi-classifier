8d1e5e369db1ff3c3d11a221b6d9b7e338a032c1
gw0 [http://gw.tnode.com/] <gw.2015@tnode.com>
2016-01-19 11:57:31 +0100

    Initial commit of readme and requirements script.

---
+++ README.md
@@ -0,0 +1,16 @@
+conll16st-multi-classifier-keras
+================================
+
+Experiments for CoNLL 2016 Supplementary Task (shallow discourse sense classification) involving deep layer-wise multi-task learning using shared representations and bidirectional GRU layers in Keras.
+
+- <http://www.cs.brandeis.edu/~clp/conll16st/>
+- <http://github.com/attapol/conll16st/>
+- <http://github.com/gw0/conll16st-multi-classifier-keras/>
+
+
+License
+=======
+
+Copyright &copy; 2016 *gw0* [<http://gw.tnode.com/>] &lt;<gw.2016@tnode.com>&gt;
+
+This code is licensed under the [GNU Affero General Public License 3.0+](LICENSE_AGPL-3.0.txt) (*AGPL-3.0+*). Note that it is mandatory to make all modifications and complete source code publicly available to any user.
---
+++ requirements.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# Requirements installation for conll16st-multi-classifier-keras
+#
+# Author: gw0 [http://gw.tnode.com/] <gw.2016@tnode.com>
+# License: All rights reserved
+
+NAME="(`basename $(realpath ${0%/*})`)"
+SRC="venv/src"
+PYTHON_EXE="python2"
+SITE_PACKAGES='venv/lib/python*/site-packages'
+DIST_PACKAGES='/usr/lib/python*/dist-packages'
+
+# Initialization
+sudo() { [ -x "/usr/bin/sudo" ] && /usr/bin/sudo "$@" || "$@"; }
+[ ! -x "/usr/bin/$PYTHON_EXE" ] && sudo apt-get install -y "$PYTHON_EXE"
+
+cd "${0%/*}"
+virtualenv --prompt="$NAME" --python="$PYTHON_EXE"  venv || exit 1
+source venv/bin/activate
+[ ! -e "$SRC" ] && mkdir "$SRC"
+
+# Prerequisites for pip
+sudo apt-get install -y git
+
+# Prerequisites for Theano
+sudo apt-get install -y python-numpy python-scipy
+[ ! -d $SITE_PACKAGES/numpy ] && cp -a $DIST_PACKAGES/numpy* $SITE_PACKAGES
+[ ! -d $SITE_PACKAGES/scipy ] && cp -a $DIST_PACKAGES/scipy* $SITE_PACKAGES
+
+# Prerequisites for Keras
+sudo apt-get install -y python-h5py libyaml-dev graphviz
+[ ! -d $SITE_PACKAGES/h5py ] && cp -a $DIST_PACKAGES/h5py* $SITE_PACKAGES
+
+# Requirements
+pip install git+https://github.com/Theano/Theano.git
+pip install pydot-ng
+pip install git+https://github.com/fchollet/keras.git
+
+# Development
+pip install pytest
+
+echo
+echo "Use: . venv/bin/activate"
+echo
