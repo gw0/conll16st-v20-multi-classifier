65b8073551a3a00858f59782c999d0c80a45db92
gw0 [http://gw.tnode.com/] <gw.2015@tnode.com>
2016-02-01 18:07:39 +0100

    Improve model01 data preparation.

--- v20/model01.py
+++ v20/model01.py
@@ -29,20 +29,20 @@ def build_model(max_len, embedding_dim, words2id_size, pos_tags2id_size, rel_typ
     shared_layers = 2
     loss = {
         #'x_skipgram': 'mse',
-        'x_pos_tags': 'binary_crossentropy',
+        #'x_pos_tags': 'binary_crossentropy',
         'x_rel_types': 'binary_crossentropy',
-        'x_rel_senses': 'binary_crossentropy',
+        #'x_rel_senses': 'binary_crossentropy',
     }
 
     # input: word ids with masked post-padding (doc, time_pad)
-    model.add_input(name='x_word_pad', input_shape=(None,), dtype='int')
+    model.add_input(name='x_words_pad', input_shape=(None,), dtype='int')
 
     # input: word ids with random post-padding (doc, time_pad)
     if 'x_skipgram' in loss:
-        model.add_input(name='x_word_rand', input_shape=(None,), dtype='int')
+        model.add_input(name='x_words_rand', input_shape=(None,), dtype='int')
 
     # shared 1: word embedding layer (doc, time_pad, emb)
-    model.add_node(Embedding(words2id_size, embedding_dim, input_length=max_len, init='glorot_uniform', mask_zero=True), name='shared_1', input='x_word_pad')
+    model.add_node(Embedding(words2id_size, embedding_dim, input_length=max_len, init='glorot_uniform', mask_zero=True), name='shared_1', input='x_words_pad')
 
     # shared N: bidirectional GRU full sequence layer (doc, time_pad, repr)
     shared_prev = 'shared_1'
@@ -57,7 +57,7 @@ def build_model(max_len, embedding_dim, words2id_size, pos_tags2id_size, rel_typ
 
     # model: skip-gram labels (sample, time_pad, offset)
     if 'x_skipgram' in loss:
-        skipgram_out = None  #XXX:skipgram_model(model, ['shared_1', 'x_word_rand'], max_len, embedding_dim, words2id_size, skipgram_offsets)
+        skipgram_out = None  #XXX:skipgram_model(model, ['shared_1', 'x_words_rand'], max_len, embedding_dim, words2id_size, skipgram_offsets)
         model.add_output(name='x_skipgram', input=skipgram_out)
 
     # model: POS tags as one-hot vectors (sample, time_pad, pos_tags2id)
@@ -81,10 +81,43 @@ def build_model(max_len, embedding_dim, words2id_size, pos_tags2id_size, rel_typ
 
 ### Prepare data
 
-def batch_generator(word_crop, max_len, batch_size, doc_ids, words, word_metas, pos_tags, dependencies, parsetrees, rel_parts, rel_types, rel_senses, (words2id, words2id_weights, words2id_size), (pos_tags2id, pos_tags2id_weights, pos_tags2id_size), (rel_types2id, rel_types2id_weights, rel_types2id_size), (rel_senses2id, rel_senses2id_weights, rel_senses2id_size)):
+def relation_sample(rel_id, word_crop, max_len, doc_ids, words, word_metas, pos_tags, dependencies, parsetrees, rel_ids, rel_parts, rel_types, rel_senses, (words2id, words2id_weights, words2id_size), (pos_tags2id, pos_tags2id_weights, pos_tags2id_size), (rel_types2id, rel_types2id_weights, rel_types2id_size), (rel_senses2id, rel_senses2id_weights, rel_senses2id_size)):
+    doc_id = rel_parts[rel_id]['DocID']
+    words_len = len(words[doc_id])
+    token_min = rel_parts[rel_id]['TokenMin']
+    token_max = rel_parts[rel_id]['TokenMax']
+
+    # determine token boundaries around each relation
+    token_start = token_min - (word_crop - (token_max - token_min) + 1) // 2
+    if token_start < 0:
+        token_start = 0
+    token_end = token_start + word_crop
+    if token_end > words_len:
+        token_start -= token_end - words_len
+        if token_start < 0:
+            token_start = 0
+        token_end = token_start + word_crop
+
+    # prepare data
+    words_slice = words[doc_id][token_start:token_end]
+    x1_words_pad = encode_x_words(words_slice, words2id, words2id_weights, words2id_size, max_len)
+    x1_words_rand = encode_x_words_rand(words_slice, words2id, words2id_weights, words2id_size, max_len)
+
+    x1_skipgram = None
+
+    pos_tags_slice = pos_tags[doc_id][token_start:token_end]
+    x1_pos_tags = encode_x_pos_tags(pos_tags_slice, pos_tags2id, pos_tags2id_weights, pos_tags2id_size, max_len)
+
+    word_metas_slice = word_metas[doc_id][token_start:token_end]
+    x1_rel_types = encode_x_rel_types(word_metas_slice, rel_types2id, rel_types2id_weights, rel_types2id_size, max_len)
+    x1_rel_senses = encode_x_rel_senses(word_metas_slice, rel_senses2id, rel_senses2id_weights, rel_senses2id_size, max_len)
+    return x1_words_pad, x1_words_rand, x1_skipgram, x1_pos_tags, x1_rel_types, x1_rel_senses, token_start, token_end
+
+
+def batch_generator(word_crop, max_len, batch_size, doc_ids, words, word_metas, pos_tags, dependencies, parsetrees, rel_ids, rel_parts, rel_types, rel_senses, (words2id, words2id_weights, words2id_size), (pos_tags2id, pos_tags2id_weights, pos_tags2id_size), (rel_types2id, rel_types2id_weights, rel_types2id_size), (rel_senses2id, rel_senses2id_weights, rel_senses2id_size)):
     """Batch generator where each sample represents a different discourse relation."""
 
-    rel_ids = rel_parts.keys()
+    rel_ids = rel_ids[:]
     while True:
         # shuffle relations on each epoch
         random.shuffle(rel_ids)
@@ -99,40 +132,22 @@ def batch_generator(word_crop, max_len, batch_size, doc_ids, words, word_metas, 
             x_rel_types = []
             x_rel_senses = []
             for rel_id in rel_ids[batch_start:batch_end]:
-                doc_id = rel_parts[rel_id]['DocID']
-                token_min = rel_parts[rel_id]['TokenMin']
-                token_max = rel_parts[rel_id]['TokenMax']
-
-                # determine token boundaries around relations in batch
-                token_start = token_min - (word_crop - (token_max - token_min) + 1) // 2
-                if token_start < 0:
-                    token_start = 0
-                token_end = token_start + word_crop
-                if token_end > len(words[doc_id]):
-                    token_start -= token_end - len(words[doc_id])
-                    if token_start < 0:
-                        token_start = 0
-                    token_end = token_start + word_crop
-
-                words_slice = words[doc_id][token_start:token_end]
-                x_words_pad.append(encode_x_words(words_slice, words2id, words2id_weights, words2id_size, max_len))
-                x_words_rand.append(encode_x_words_rand(words_slice, words2id, words2id_weights, words2id_size, max_len))
-
-                pos_tags_slice = pos_tags[doc_id][token_start:token_end]
-                x_pos_tags.append(encode_x_pos_tags(pos_tags_slice, pos_tags2id, pos_tags2id_weights, pos_tags2id_size, max_len))
-
-                word_metas_slice = word_metas[doc_id][token_start:token_end]
-                x_rel_types.append(encode_x_rel_types(word_metas_slice, rel_types2id, rel_types2id_weights, rel_types2id_size, max_len))
-                x_rel_senses.append(encode_x_rel_senses(word_metas_slice, rel_senses2id, rel_senses2id_weights, rel_senses2id_size, max_len))
+                x1_words_pad, x1_words_rand, x1_skipgram, x1_pos_tags, x1_rel_types, x1_rel_senses, token_start, token_end = relation_sample(rel_id, word_crop, max_len, doc_ids, words, word_metas, pos_tags, dependencies, parsetrees, rel_ids, rel_parts, rel_types, rel_senses, (words2id, words2id_weights, words2id_size), (pos_tags2id, pos_tags2id_weights, pos_tags2id_size), (rel_types2id, rel_types2id_weights, rel_types2id_size), (rel_senses2id, rel_senses2id_weights, rel_senses2id_size))
+                x_words_pad.append(x1_words_pad)
+                x_words_rand.append(x1_words_rand)
+                x_skipgram.append(x1_skipgram)
+                x_pos_tags.append(x1_pos_tags)
+                x_rel_types.append(x1_rel_types)
+                x_rel_senses.append(x1_rel_senses)
 
             # yield batch
             yield {
-                'x_words_pad': x_words_pad,
-                'x_words_rand': x_words_rand,
-                'x_skipgram': x_skipgram,
-                'x_pos_tags': x_pos_tags,
-                'x_rel_types': x_rel_types,
-                'x_rel_senses': x_rel_senses,
+                'x_words_pad': np.asarray(x_words_pad, dtype=np.int),
+                #'x_words_rand': np.asarray(x_words_rand, dtype=np.int),
+                #'x_skipgram': np.asarray(x_skipgram, dtype=np.float32),
+                #'x_pos_tags': np.asarray(x_pos_tags, dtype=np.float32),
+                'x_rel_types': np.asarray(x_rel_types, dtype=np.float32),
+                #'x_rel_senses': np.asarray(x_rel_senses, dtype=np.float32),
             }
 
 
